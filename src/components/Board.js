32783d04a29bbc6d797f246ca900034bee1e33a3
Jordan Jasik <jordanj@oneclickcontractor.com>
2023-12-04 21:26:51 -0500

    dont override moves

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
   function handleClick(index){
+    if(squares[index]){
+      return;
+    }
     const nextSquares = squares.slice();
     if(xTurn){
       nextSquares[index] = "X";
