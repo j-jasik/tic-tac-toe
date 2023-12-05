ce86da0f3b45befe263ea669f2e28769fc0de190
Jordan Jasik <jordanj@oneclickcontractor.com>
2023-12-04 21:21:30 -0500

    add board component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import { useState } from 'react';
-import Square from './components/Square'
+import Board from './components/Board'
 
-export default function Board() {
-  const [xTurn, setXTurn] = useState(true)
-  const [squares, setSquares] = useState(Array(9).fill(null))
-
-  function handleClick(index){
-    const nextSquares = squares.slice();
-    if(xTurn){
-      nextSquares[index] = "X";
-    }else{
-      nextSquares[index] = "O";
-    }
-    setSquares(nextSquares);
-    setXTurn(!xTurn);
-  }
+export default function App() {
 
   return (
     <>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <Board />
     </>
-
   );
 }
